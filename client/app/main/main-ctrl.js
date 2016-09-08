e7b02d65634a1cd20b43c687ca234cb71a290486
Chase Ramsey <ramsey.chase@gmail.com>
2016-09-08 10:32:43 -0500

    Created main factory; filter vars in controller

--- client/app/main/main-ctrl.js
+++ client/app/main/main-ctrl.js
@@ -1,5 +1,14 @@
-app.controller('MainCtrl', ['$scope', function($scope) {
+app.controller('MainCtrl', ['$scope', 'MainFactory', function($scope, MainFactory) {
 
     $scope.title = 'LL COOL SRC';
+
+// Filters for what displays on page
+    $scope.showArtists = null;  // Boolean; shows only artists
+    $scope.filterArtist = null; // Artist; shows tracks by selected artist
+
+    $scope.showAlbums = null;  // Boolean; shows only albums
+    $scope.filterAlbum = null; // Album; shows tracks on the selected album
+
+    $scope.showTracks = null;  // Boolean; shows all tracks
 
 }]);
