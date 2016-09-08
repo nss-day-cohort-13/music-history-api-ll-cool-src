8c4c0ce4f692ea9066e386d379ca9bf58e0afe10
Chase Ramsey <ramsey.chase@gmail.com>
2016-09-08 13:14:25 -0500

    Streamlined length-filter

--- client/app/length-filter.js
+++ client/app/length-filter.js
@@ -1,15 +1,10 @@
 app.filter('lengthFilter', () => {
     return (timeSeconds) => {
-        let hours = Math.floor(timeSeconds / 3600);
         let minutes = Math.floor(timeSeconds / 60);
         let seconds = timeSeconds - (minutes * 60);
         if (seconds < 10) {
             seconds = '0' + seconds.toString();
         }
-        if (hours > 0) {
-            return `${hours}:${minutes}:${seconds}`
-        } else {
-            return `${minutes}:${seconds}`
-        }
+        return `${minutes}:${seconds}`
     }
 })
