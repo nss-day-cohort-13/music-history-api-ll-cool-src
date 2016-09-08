caaabe78a904c02026862a2cfab6c8d0407ec669
Chase Ramsey <ramsey.chase@gmail.com>
2016-09-08 13:09:04 -0500

    Ng clicks in nav show correct tables

--- client/app/main/main-ctrl.js
+++ client/app/main/main-ctrl.js
@@ -45,6 +45,7 @@ app.controller('MainCtrl', ['$scope', '$timeout', '$http', 'MainFactory', functi
         $scope.showArtist = null;
         $scope.showAlbum = null;
         $scope.showCategory = category;
+        console.log("category: ", $scope.showCategory);
     }
 
     $scope.showArtistAlbums = (artist) => {
